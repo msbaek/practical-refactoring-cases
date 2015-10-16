333180b807d74882f5d0567cc8765803556fbe76
mikael.baek <mikael.baek@daumkakao.com>
2015-10-16 11:26:23 +0900

    add failing test for POST method

--- src/main/java/ocp/README.md
+++ src/main/java/ocp/README.md
@@ -1,6 +1,14 @@
-# 최초 요구사항
+# 1. 최초 요구사항
 
 GET 메소드로 http request를 요청하고 결과를 반환하는 요구사항
 
 ![](http://i.imgur.com/dOe9DnP.png)
 
+# 2. POST 메소드 처리 추가 요청 사항
+
+언제나 그렇듯 요구사항은 항상 변한다.
+
+기존 GET 메소드 처리 외에 POST 메소드 처리가 추가로 요구되었다.
+
+### 2.1 failing test 추가
+![image](http://i.imgur.com/iiM46dN.png)
--- src/test/java/ocp/HttpRequestExecutorTest.java
+++ src/test/java/ocp/HttpRequestExecutorTest.java
@@ -1,9 +1,11 @@
 package ocp;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.function.BooleanSupplier;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.StringStartsWith.startsWith;
@@ -11,11 +13,24 @@ import static org.hamcrest.core.StringStartsWith.startsWith;
 public class HttpRequestExecutorTest {
     private String requestURI = "http://www.daum.net";
     private Map<String,String> params;
+    private Boolean isGet = true;
+    private HttpRequestExecutor executor;
+
+    @Before
+    public void setUp() throws Exception {
+        executor = new HttpRequestExecutor();
+    }
 
     @Test
-    public void foo() throws IOException {
-        HttpRequestExecutor executor = new HttpRequestExecutor();
-        ResponseModel responseModel = executor.handleRequest(requestURI, params);
+    public void get() throws IOException {
+        ResponseModel responseModel = executor.handleRequest(isGet, requestURI, params);
+        assertThat(responseModel.getResponseAsText(), startsWith("<!DOCTYPE html><html lang=\"ko\"><head><meta charset=\"utf-8\"><title>Daum &ndash; 모으다 잇다 흔들다</title"));
+    }
+
+    @Test
+    public void post() throws IOException {
+        isGet = false;
+        ResponseModel responseModel = executor.handleRequest(isGet, requestURI, params);
         assertThat(responseModel.getResponseAsText(), startsWith("<!DOCTYPE html><html lang=\"ko\"><head><meta charset=\"utf-8\"><title>Daum &ndash; 모으다 잇다 흔들다</title"));
     }
 }
